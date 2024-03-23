80d9958aba5e99cde321d9e2de8162bc9c2fcb50
almamun203 <alauddinalmamun203@gmail.com>
2024-03-23 10:41:07 +0600

    Added some Components

---
+++ src/Components/CategoryList/CategoryList.jsx
@@ -0,0 +1,11 @@
+
+
+const CategoryList = () => {
+    return (
+        <div>
+            <h2 className="text-4xl text-center">Job Category List</h2>
+        </div>
+    );
+};
+
+export default CategoryList;
