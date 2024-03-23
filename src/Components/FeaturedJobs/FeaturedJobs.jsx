77256c1e391a2d2fd86bc6537b0cb0830990de83
almamun203 <alauddinalmamun203@gmail.com>
2024-03-23 19:49:07 +0600

    Featured Jobs Section Added

--- src/Components/FeaturedJobs/FeaturedJobs.jsx
+++ src/Components/FeaturedJobs/FeaturedJobs.jsx
@@ -6,6 +6,8 @@ const FeaturedJobs = () => {
 
 const [jobs,setJobs]=useState([]);
 
+const [dataLength,setDataLength]=useState(4);
+
 useEffect(()=>{
     fetch('jobs.json')
     .then(res=>res.json())
@@ -19,13 +21,16 @@ useEffect(()=>{
              <h2 className="font-bold  my-4 text-4xl text-center">Featured Jobs</h2>
             <p className="text-center text-sm text-slate-400 font-medium">Explore thousands of job opportunities with all the information you need.Its your future.</p>
         </div>
-        <div>
+        <div className="grid lg:grid-cols-3 grid-cols-1 md:grid-cols-2 sm:justify-center gap-x-5 ">
             {
-                jobs.map((job)=><Job
+                jobs.slice(0,dataLength).map((job)=><Job
                 key={job.id}
                 job={job}
                 ></Job>)
             }
+        </div>
+        <div className="text-center my-4">
+            <button className=" btn bg-indigo-400 text-white btn-sm " onClick={()=>setDataLength()}>Show All Jobs</button>
         </div>
         </div>
     );
--- src/Components/Job/Job.jsx
+++ src/Components/Job/Job.jsx
@@ -1,14 +1,27 @@
 import PropTypes from 'prop-types';
-
+import { CiLocationOn } from "react-icons/ci";
+import { MdAttachMoney } from "react-icons/md";
 const Job = ({job}) => {
-    const{logo,job_title,company_name,job_type,location}=job;
+    const{logo,job_title,company_name,job_type,location,remote_or_onsite,salary}=job;
+
     return (
-        <div>
-            <h2>{job_title}</h2>
+        <div className='border-2 rounded-md mt-5 p-5 hover:outline-2  hover:outline-double hover:outline-indigo-400'>
+            
             <img src={logo} alt="" />
-            <h2>{company_name}</h2>
-            <p>{job_type}</p>
-            <p>{location}</p>
+            <h2 className='font-bold'>{job_title}</h2>
+            <h2 className='text-sm font-semibold
+             text-slate-700'>{company_name}</h2>
+            <div className='flex gap-3 my-2'>
+            <p className='text-xs  font-medium  border-2 rounded-sm border-fuchsia-400 text-fuchsia-400 px-2 py-1'>{job_type}</p>
+            <p className='text-xs  font-medium border-2 rounded-sm border-fuchsia-400 text-fuchsia-400 px-2 py-1'>{remote_or_onsite}</p>
+            </div>
+            <div className='flex gap-3 font-semibold'>
+            <p className='text-xs flex gap-1 items-center'> <span><CiLocationOn></CiLocationOn></span>
+            {location}</p>
+            <p className='text-xs flex gap-1 items-center'><span><MdAttachMoney></MdAttachMoney></span>
+            {salary}</p>
+            </div>
+            <button className="btn bg-indigo-400 text-white  btn-sm mt-2">View Details</button>
         </div>
     );
 };
